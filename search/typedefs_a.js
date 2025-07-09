5cb19bee338ed87da0fde44797e3fbfae76607b4
loopset <miguellozanogonzalez119@gmail.com>
2025-07-09 10:49:55 +0000

    Deploying to gh-pages from @ loopset/ActRoot@bcbcdbfc891314852372766c46e1198bd7883f00 🚀

--- search/typedefs_a.js
+++ search/typedefs_a.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['rangepair_0',['RangePair',['../classActRoot_1_1Region.html#a9f5bf795481d58597345c02df6ccec80',1,'ActRoot::Region']]],
   ['rangetype_1',['RangeType',['../classActRoot_1_1Cluster.html#a73bf59dd7232299a1d6e889a73636007',1,'ActRoot::Cluster']]],
-  ['regionpair_2',['RegionPair',['../classActRoot_1_1Region.html#a2149ccc5f4266e1cd1e052b6db124622',1,'ActRoot::Region']]],
-  ['rpcluster_3',['RPCluster',['../classActAlgorithm_1_1MultiStep.html#a1e332bbb28fcff4dc1c2bd80fb4d0fd4',1,'ActAlgorithm::MultiStep']]],
-  ['rpelement_4',['RPElement',['../namespaceActAlgorithm.html#abd67f26f7a756905fc602ec06cf170f8',1,'ActAlgorithm']]],
-  ['rpset_5',['RPSet',['../namespaceActAlgorithm.html#a5efa1e274a84a167f1a5a3086558c6ae',1,'ActAlgorithm']]],
-  ['rpvalue_6',['RPValue',['../classActAlgorithm_1_1MultiStep.html#a7ae833fa5c1961a3621ae0bc1a89074f',1,'ActAlgorithm::MultiStep']]],
-  ['rpvector_7',['RPVector',['../namespaceActAlgorithm.html#a25ab775a4c2a4cfb2a6466ebb2b11a27',1,'ActAlgorithm']]]
+  ['regionmap_2',['RegionMap',['../classActAlgorithm_1_1Actions_1_1SplitRegion.html#a7152fb214c5b135f557e070cd5d50598',1,'ActAlgorithm::Actions::SplitRegion']]],
+  ['regionpair_3',['RegionPair',['../classActRoot_1_1Region.html#a2149ccc5f4266e1cd1e052b6db124622',1,'ActRoot::Region']]],
+  ['rpcluster_4',['RPCluster',['../classActAlgorithm_1_1MultiStep.html#a1e332bbb28fcff4dc1c2bd80fb4d0fd4',1,'ActAlgorithm::MultiStep']]],
+  ['rpelement_5',['RPElement',['../namespaceActAlgorithm.html#abd67f26f7a756905fc602ec06cf170f8',1,'ActAlgorithm']]],
+  ['rpset_6',['RPSet',['../namespaceActAlgorithm.html#a5efa1e274a84a167f1a5a3086558c6ae',1,'ActAlgorithm']]],
+  ['rpvalue_7',['RPValue',['../classActAlgorithm_1_1MultiStep.html#a7ae833fa5c1961a3621ae0bc1a89074f',1,'ActAlgorithm::MultiStep']]],
+  ['rpvector_8',['RPVector',['../namespaceActAlgorithm.html#a25ab775a4c2a4cfb2a6466ebb2b11a27',1,'ActAlgorithm']]]
 ];
