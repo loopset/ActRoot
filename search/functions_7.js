d71953a0014219ddbd9d5fa545169dec09b028c5
loopset <miguellozanogonzalez119@gmail.com>
2025-07-11 09:05:47 +0000

    Deploying to gh-pages from @ loopset/ActRoot@7257bfaa545d6cc3738c4a539d613594366c0dd3 🚀

--- search/functions_7.js
+++ search/functions_7.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['hasaction_0',['HasAction',['../classActAlgorithm_1_1MultiAction.html#ae9180962230781d47edc76b78ea7675e',1,'ActAlgorithm::MultiAction']]],
   ['hasrunchanged_1',['HasRunChanged',['../classActRoot_1_1InputIterator.html#aedd4c67b2ad9ac56152d494f9c2ebd77',1,'ActRoot::InputIterator']]],
-  ['help_2',['Help',['../classActRoot_1_1Options.html#a7a0d054a8b424870fb246868f314e588',1,'ActRoot::Options']]],
-  ['histogrampainter_3',['HistogramPainter',['../classActRoot_1_1HistogramPainter.html#a8efffd4ea6ab02459206bbe150a39f53',1,'ActRoot::HistogramPainter']]]
+  ['hassp_2',['HasSP',['../classActRoot_1_1BinaryData.html#ac37368945ee2db913de057ea80ae713a',1,'ActRoot::BinaryData']]],
+  ['help_3',['Help',['../classActRoot_1_1Options.html#a7a0d054a8b424870fb246868f314e588',1,'ActRoot::Options']]],
+  ['histogrampainter_4',['HistogramPainter',['../classActRoot_1_1HistogramPainter.html#a8efffd4ea6ab02459206bbe150a39f53',1,'ActRoot::HistogramPainter']]]
 ];
