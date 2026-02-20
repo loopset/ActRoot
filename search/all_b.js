073cc2ba524058188b7ef15702f0d185d4c5a304
ibc12 <108010970+ibc12@users.noreply.github.com>
2026-02-20 12:09:22 +0000

    Deploying to gh-pages from @ loopset/ActRoot@fe553f3dec4497e1d0ad8f575df2fba84117a935 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -5,7 +5,7 @@ var searchData=
   ['legendangular_2',['legendAngular',['../classActSim_1_1TheoCrossSection.html#a9c14f07dfc690285ef05c4ac792a2c3a',1,'ActSim::TheoCrossSection']]],
   ['legendrecoeffs_3',['legendreCoeffs',['../classActSim_1_1TheoCrossSection.html#a2ad4ff00ea0149d9d0498adbbdf93e24',1,'ActSim::TheoCrossSection']]],
   ['length_4',['Length',['../classActAlgorithm_1_1Interval.html#a03c96ef51b498a280a11a10b9f01e625',1,'ActAlgorithm::Interval']]],
-  ['lightorheavy_5',['LightOrHeavy',['../classActRoot_1_1MergerDetector.html#a6e648156c52ebf690dd9c049060c9fd6',1,'ActRoot::MergerDetector']]],
+  ['lightorheavy_5',['LightOrHeavy',['../classActRoot_1_1MergerDetector.html#aa99ecb62fb2c15c055be700dc0d4b7d6',1,'ActRoot::MergerDetector']]],
   ['line_6',['line',['../classActRoot_1_1Line.html',1,'ActRoot::Line'],['../classActRoot_1_1Line.html#aec3e244591a6afb9ee2e18b44063fb76',1,'ActRoot::Line::Line()=default'],['../classActRoot_1_1Line.html#a7bade61caa570696be228264b32306a8',1,'ActRoot::Line::Line(XYZPointF point, XYZVectorF direction, float chi)'],['../classActRoot_1_1Line.html#aa0e552a33685ed263731bd94a975c745',1,'ActRoot::Line::Line(const XYZPointF &amp;p1, const XYZPointF &amp;p2)']]],
   ['linemap_7',['LineMap',['../classActRoot_1_1HistogramPainter.html#abd0104d4af047797df62acab8dc221b2',1,'ActRoot::HistogramPainter']]],
   ['loadplugin_8',['LoadPlugin',['../classActAlgorithm_1_1TaskManager.html#a5ce7d3bd2f20c41bc75abc03f33d1870',1,'ActAlgorithm::TaskManager']]],
